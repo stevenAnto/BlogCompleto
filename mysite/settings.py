36956c9667cd47921dd7f4e3518dba7d16e0f729
stevenAnto <ecalcinap@unsa.edu.pe>
2022-06-14 18:04:03 -0500

    faltaba un punto

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xz88e85khbe!ordo3yj%+sy(04if-8x7k+3b&j+k=+p)xsh^h8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
